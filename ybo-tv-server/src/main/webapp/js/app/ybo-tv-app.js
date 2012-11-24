1b697b0909f7e0260b4e1636e791f8e05cb700c6
ybonnel <ybonnel@gmail.com>
2012-11-25 00:36:24 +0100

    Ajout d'une message de chargement

--- ybo-tv-server/src/main/webapp/js/app/ybo-tv-app.js
+++ ybo-tv-server/src/main/webapp/js/app/ybo-tv-app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-var App = angular.module('YboTv', ['YboTv.services']);
+var App = angular.module('YboTv', ['YboTv.services', 'loading']);
 
 
 // Configuration des routes
@@ -15,3 +15,45 @@ App.config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/now'});
   }]);
 
+
+
+var mod = angular.module('loading', []);
+
+mod.factory('loadingService', function() {
+    var service = {
+        requestCount: 0,
+        isLoading: function() {
+            return service.requestCount > 0;
+        }
+    };
+    return service;
+});
+
+mod.factory('onStartInterceptor', function(loadingService) {
+    return function (data, headersGetter) {
+        loadingService.requestCount++;
+        return data;
+    };
+});
+
+mod.factory('onCompleteInterceptor', function(loadingService) {
+    return function(promise) {
+        var decrementRequestCount = function(response) {
+            loadingService.requestCount--;
+            return response;
+        };
+        return promise.then(decrementRequestCount, decrementRequestCount);
+    };
+});
+
+mod.config(function($httpProvider) {
+    $httpProvider.responseInterceptors.push('onCompleteInterceptor');
+});
+
+mod.run(function($http, onStartInterceptor) {
+    $http.defaults.transformRequest.push(onStartInterceptor);
+});
+
+mod.controller('LoadingCtrl', function($scope, loadingService) {
+    $scope.$watch(function() { return loadingService.isLoading(); }, function(value) { $scope.loading = value; });
+});
