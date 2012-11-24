ae9f20c483f6b9718540ee86380e25b8d90e073e
ybonnel <ybonnel@gmail.com>
2012-11-24 18:48:26 +0100

    Add active to navs

--- ybo-tv-server/src/main/webapp/js/app/ybo-tv-controllers.js
+++ ybo-tv-server/src/main/webapp/js/app/ybo-tv-controllers.js
@@ -1,19 +1,25 @@
 'use strict';
 
+
 /* Controllers */
 function NowController($scope, ChannelService, ProgrammeService, $log, $location) {
     var sdf = new JsSimpleDateFormat("yyyyMMddHHmmss");
     var now = sdf.format(new Date());
     $scope.channels = ProgrammeService.getByDate(now);
+    $('#cesoir').removeClass('active');
+    $('#now').addClass('active');
 }
 // Pour que l'injection de dépendances fonctionne en cas de 'minifying'
-NowController().$inject = ['$scope', 'ChannelService', 'ProgrammeService', '$log', '$location'];
+NowController.$inject = ['$scope', 'ChannelService', 'ProgrammeService', '$log', '$location'];
 
 /* Controllers */
 function CeSoirController($scope, ChannelService, ProgrammeService, $log, $location) {
+    $scope.currentUrl = $location.url();
     var sdf = new JsSimpleDateFormat("yyyyMMdd");
     var now = sdf.format(new Date()) + '210000';
     $scope.channels = ProgrammeService.getByDate(now);
+    $('#now').removeClass('active');
+    $('#cesoir').addClass('active');
 }
 // Pour que l'injection de dépendances fonctionne en cas de 'minifying'
-CeSoirController().$inject = ['$scope', 'ChannelService', 'ProgrammeService', '$log', '$location'];
+CeSoirController.$inject = ['$scope', 'ChannelService', 'ProgrammeService', '$log', '$location'];
