6fe98b0a149fdf40f1a557a7a38b240df1039f03
ybonnel <ybonnel@sopragroup.com>
2012-11-29 17:40:43 +0100

    Fix #14 : ajout du principe de change log

---
+++ ybo-tv-android/src/main/java/fr/ybo/ybotv/android/util/ChangeLogDialog.java
@@ -0,0 +1,11 @@
+package fr.ybo.ybotv.android.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ybonnel
+ * Date: 29/11/12
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChangeLogDialog {
+}
