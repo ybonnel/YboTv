c1bc44a2794e0049d5fd81ad986ce5c07084ec00
ybonnel <ybonnel@sopragroup.com>
2012-11-29 15:53:59 +0100

    Fix #2 : no problem detected.

--- ybo-tv-android/src/main/java/fr/ybo/ybotv/android/YboTvApplication.java
+++ ybo-tv-android/src/main/java/fr/ybo/ybotv/android/YboTvApplication.java
@@ -20,6 +20,12 @@ public class YboTvApplication extends Application {
                 .detectAll()
                 .penaltyLog()
                 .penaltyDeath()
+                .build());
+
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDeath()
                 .build());*/
     }
 
