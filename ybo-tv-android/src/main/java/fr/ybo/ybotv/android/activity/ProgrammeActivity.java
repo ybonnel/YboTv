06c367449f189bc532e9ed5437bb33905bae5ef3
ybonnel <ybonnel@sopragroup.com>
2012-11-29 14:19:09 +0100

    Fix #3 : amelioration de l'affichage du programme

--- ybo-tv-android/src/main/java/fr/ybo/ybotv/android/activity/ProgrammeActivity.java
+++ ybo-tv-android/src/main/java/fr/ybo/ybotv/android/activity/ProgrammeActivity.java
@@ -1,6 +1,7 @@
 package fr.ybo.ybotv.android.activity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,9 +12,18 @@ import fr.ybo.ybotv.android.lasylist.ImageLoader;
 import fr.ybo.ybotv.android.modele.ChannelWithProgramme;
 import fr.ybo.ybotv.android.modele.Programme;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProgrammeActivity extends SherlockActivity {
+
+    private final static Map<String, Integer> mapOfRatings = new HashMap<String, Integer>(){{
+        put("1/4", R.drawable.rating_1star);
+        put("2/4", R.drawable.rating_2star);
+        put("3/4", R.drawable.rating_3star);
+        put("4/4", R.drawable.rating_4star);
+    }};
 
 
     @Override
@@ -24,6 +34,7 @@ public class ProgrammeActivity extends SherlockActivity {
         ImageLoader imageLoader=new ImageLoader(getApplicationContext());
 
         ImageView icon = (ImageView) findViewById(R.id.programme_activity_icon);
+        ImageView rating = (ImageView) findViewById(R.id.programme_activity_rating);
         TextView title = (TextView) findViewById(R.id.programme_activity_title);
         TextView description = (TextView) findViewById(R.id.programme_activity_description);
 
@@ -33,8 +44,22 @@ public class ProgrammeActivity extends SherlockActivity {
         } else {
             icon.setVisibility(View.GONE);
         }
+        if (programme.getStarRating() != null
+                && mapOfRatings.containsKey(programme.getStarRating())) {
+            rating.setImageResource(mapOfRatings.get(programme.getStarRating()));
+            rating.setVisibility(View.VISIBLE);
+        } else {
+            rating.setVisibility(View.GONE);
+        }
         title.setText(programme.getTitle());
-        description.setText(programme.getDesc());
+        if (programme.getDesc() != null) {
+            description.setText(programme.getDesc());
+            description.setVisibility(View.VISIBLE);
+        } else {
+            description.setVisibility(View.GONE);
+        }
+
+        description.setMovementMethod(new ScrollingMovementMethod());
 
     }
 
