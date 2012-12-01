5a9e95e76bcc8c0e5d298476c92c150b36f9d8fd
ybonnel <ybonnel@gmail.com>
2012-12-01 16:30:40 +0100

    Change version in deploy.sh

--- ybo-tv-android/deploy.sh
+++ ybo-tv-android/deploy.sh
@@ -1,3 +1,3 @@
 mvn clean install
-adb install -r target/ybo-tv-android-1.0-SNAPSHOT.apk
+adb install -r target/ybo-tv-android-1.1-SNAPSHOT.apk
 adb shell am start -n fr.ybo.ybotv.android/.activity.LoadingActivity
